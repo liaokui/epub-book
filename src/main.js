508c7d974a9c5bbb93ccd7efa8017a6312bf146c
liaokui <13047903316lelk@gmail.com>
2020-03-24 23:55:42 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.scss'
+
+import { Button, Card, Form, FormItem, Input, Loading, Message} from 'element-ui';
+// import 'normalize.css'
+
+Vue.prototype.$ELEMENT = {  zIndex: 3000 };
+Vue.use(Button)
+Vue.use(Card)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Loading)
+
+Vue.prototype.$message = Message;
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Login = () => import('../views/login/login.vue');
+// const List = () => import('../views/list/list.vue');
+// const Detail = () => import('../views/detail/detail.vue');
+// const Error = () => import('../views/error/error');
+
+
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        title: ['登录'],
+      },
+    },
+    // {
+    //   path: '/list',
+    //   name: 'list',
+    //   component: List,
+    //   meta: {
+    //     title: ['书籍列表'],
+    //   },
+    // },
+    // {
+    //   path: '/detail',
+    //   name: 'detail',
+    //   component: Detail,
+    //   meta: {
+    //     title: ['书籍详情'],
+    //   },
+    // },
+    // {
+    //   path: '**',
+    //   name: 'error',
+    //   component: Error,
+    //   meta: {
+    //     title: ['错误'],
+    //   },
+    // }
+  ]
+})
+
