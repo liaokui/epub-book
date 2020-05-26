b48ec25ba910cd9f05e856cfba54f0d08366efd0
liaokui <13047903316lelk@gmail.com>
2020-05-26 09:32:35 +0800

    测试publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './public/client/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   // publicPath: '/',
   assetsDir: 'static',
   lintOnSave: true,
